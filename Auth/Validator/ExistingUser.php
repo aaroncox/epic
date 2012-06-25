b96add1c2c641910e38690dc2a8cfed230f29fdc
Aaron Cox <aaron.cox@greymass.com>
2012-06-25 15:37:34 -0500

    Case fix

--- Auth/Validator/ExistingUser.php
+++ Auth/Validator/ExistingUser.php
@@ -26,7 +26,7 @@ class Epic_Auth_Validator_ExistingUser extends Zend_Validate_Abstract
 	public function isValid($value)
 	{
 		$this->_setValue($value);
-		if(!Epic_Mongo::db('user')->fetchOne(array('username' => $value))) {
+		if(!Epic_Mongo::db('user')->fetchOne(array('username' => strtolower($value)))) {
 			$this->_error(self::DOESNT_EXISTS);
     	return false;
 		}
