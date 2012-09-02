83d4cda1982ae6a1d25ec9679b44b4e1ab203997
Aaron Cox <aaron.cox@greymass.com>
2012-09-02 12:13:29 -0500

    Trimming on username

--- Auth/Validator/Username.php
+++ Auth/Validator/Username.php
@@ -26,7 +26,7 @@ class Epic_Auth_Validator_Username extends Zend_Validate_Abstract
 	public function isValid($value)
 	{
 		$this->_setValue($value);
-		if(Epic_Mongo::db('user')->fetchOne(array('username' => $value))) {
+		if(Epic_Mongo::db('user')->fetchOne(array('username' => trim($value)))) {
 			$this->_error(self::ALREADY_EXISTS);
     	return false;
 		}
